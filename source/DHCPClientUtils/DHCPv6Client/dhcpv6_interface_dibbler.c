7f87679825b0ea9d48460b3534566183679cfedb
jgopi571 <Jayajothi_Gopi@comcast.com>
2026-02-10 05:59:20 +0000

    CIECCPE-681: Testing purpose only
    Reason for change: For test purpose
    Test Procedure: For test purpose
    Risks: Low
    Priority: P1
    Signed-off-by:Jayajothi_Gopi@comcast.com

--- source/DHCPClientUtils/DHCPv6Client/dhcpv6_interface_dibbler.c
+++ source/DHCPClientUtils/DHCPv6Client/dhcpv6_interface_dibbler.c
@@ -427,7 +427,7 @@ pid_t start_dhcpv6_client(char *interfaceName, dhcp_opt_list *req_opt_list, dhcp
 
     char cmd_args[BUFLEN_256] = {0};
     snprintf(cmd_args, sizeof(cmd_args), "%s -w %s", DIBBLER_CLIENT_RUN_CMD, config_path);
-    sleep(2);
+    sleep(3);
     pid_t ret = start_exe2(DIBBLER_CLIENT_PATH, cmd_args);
     if (ret <= 0)
     {
