365a4e03f93a0ac348efbde1a6eeb46ea3f63d82
aadhithan01 <64963550+aadhithan01@users.noreply.github.com>
2026-09-14 14:56:28 +0530

    Force stop unresponsive Dibbler clients
    Verify the process identity before falling back to SIGKILL so restart
    requests cannot target a stale or reused PID.
    
    Signed-off-by: aadhithan01 <64963550+aadhithan01@users.noreply.github.com>

--- source/DHCPClientUtils/DHCPv6Client/dhcpv6_interface_dibbler.c
+++ source/DHCPClientUtils/DHCPv6Client/dhcpv6_interface_dibbler.c
@@ -36,10 +36,39 @@
 #define DIBBLER_PLUGIN_EXE                "/usr/bin/dhcpmgr_dibbler_plugin"
 #define DIBBLER_LOG_CONFIG                "log-level 7\nlog-mode full\n"
 #define DIBBLER_DUID_LL_CONFIG            "duid-type duid-ll\n"  
-#define DIBBLER_CLIENT_TERMINATE_INTERVAL (0.5 * MSECS_IN_SEC)
+#define DIBBLER_CLIENT_TERMINATE_INTERVAL (MSECS_IN_SEC / 2)
 #define DIBBLER_TMP_DIR_PATH              "/var/lib/dibbler"
 #define DIBBLER_RADVD_FILE                "/etc/dibbler/radvd.conf"
 #define DIBBLER_RADVD_FILE_OLD            "/etc/dibbler/radvd.conf.old"
+
+static bool is_process_running(pid_t pid, const char *process_name)
+{
+    char path[BUFLEN_64] = {0};
+    char name[BUFLEN_64] = {0};
+    snprintf(path, sizeof(path), "/proc/%d/comm", pid);
+
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL)
+    {
+        return false;
+    }
+
+    bool running = fgets(name, sizeof(name), fp) != NULL;
+    fclose(fp);
+    name[strcspn(name, "\n")] = '\0';
+    return running && strcmp(name, process_name) == 0;
+}
+
+static bool wait_for_process_exit(pid_t pid, const char *process_name, unsigned int timeout)
+{
+    while (timeout > 0 && is_process_running(pid, process_name))
+    {
+        unsigned int wait_time = timeout > DIBBLER_CLIENT_TERMINATE_INTERVAL ? DIBBLER_CLIENT_TERMINATE_INTERVAL : timeout;
+        usleep(wait_time * USECS_IN_MSEC);
+        timeout -= wait_time;
+    }
+    return !is_process_running(pid, process_name);
+}
 
 static int copy_file (char * src, char * dst)
 {
@@ -515,9 +544,21 @@ int send_dhcpv6_release(pid_t processID) {
         DHCPMGR_LOG_ERROR("%s %d: unable to send signal to pid %d\n", __FUNCTION__, __LINE__, processID);
          return FAILURE;
     }
-    struct timespec ts = {2, 0};
-    nanosleep(&ts, NULL);
-    //TODO: start_exe2 will add a sigchild handler, Do we still require this call ?
-    int ret = collect_waiting_process(processID, DIBBLER_CLIENT_TERMINATE_TIMEOUT);
-    return ret;
+    if (wait_for_process_exit(processID, DIBBLER_CLIENT, DIBBLER_CLIENT_TERMINATE_TIMEOUT))
+    {
+        return SUCCESS;
+    }
+
+    DHCPMGR_LOG_WARNING("%s %d: dibbler-client still running. Sending SIGKILL to pid %d\n", __FUNCTION__, __LINE__, processID);
+    if (signal_process(processID, SIGKILL) != RETURN_OK)
+    {
+        DHCPMGR_LOG_ERROR("%s %d: unable to send SIGKILL to pid %d\n", __FUNCTION__, __LINE__, processID);
+        return FAILURE;
+    }
+    if (!wait_for_process_exit(processID, DIBBLER_CLIENT, 1000))
+    {
+        DHCPMGR_LOG_ERROR("%s %d: dibbler-client pid %d still running after SIGKILL\n", __FUNCTION__, __LINE__, processID);
+        return FAILURE;
+    }
+    return SUCCESS;
 }
